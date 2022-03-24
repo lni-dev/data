797e2a7b44f4cfd165d1f143a7a400493889146c
LinusDev <einsuperlinus@gmail.com>
2022-03-24 19:14:19 +0100

    Library

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module me.linusdev.data {
+    exports me.linusdev.data;
+    exports me.linusdev.data.parser;
+    exports me.linusdev.data.parser.exceptions;
+    exports me.linusdev.data.converter;
+    requires org.jetbrains.annotations;
+}
