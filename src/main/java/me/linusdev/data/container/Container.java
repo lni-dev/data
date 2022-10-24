f344849c0a201205dac129a59b5884d0226dace1
LinusDev <info.linusdev@gmail.com>
2022-10-24 18:09:34 +0200

    added docs

--- src/main/java/me/linusdev/data/container/Container.java
+++ src/main/java/me/linusdev/data/container/Container.java
@@ -28,53 +28,180 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * This class holds a value from an {@link AbstractData} (called parent data). It
+ * offers several methods to process this value. All these methods will not have any effect on the parent data.
+ * The value can be accessed using the {@link #get()} method and processed with the {@link #process(Consumer)} method.
+ * <br><br>
+ * <h3>Code examples</h3>
+ * The following code will throw an exception if the json did not contain a key "some_key" or if its value was {@code null}.
+ * Otherwise, it will just print the value of "some_key":
+ * <pre>{@code
+ *  JsonParser parser = new JsonParser();
+ *  SOData data = parser.parseReader(new StringReader("{...}"));
+ *
+ *  data.getContainer("some_key")
+ *          .requireNotNull()
+ *          .process((Object o) -> System.out.println("some_key: " + o));
+ * }</pre>
+ * <br>
+ * The following code will do nothing if the key "some_value" does not exist. It throws an exception if the key does
+ * exist, but its value is {@code null}. And it will print the value of "some_key" if this exists and its value is not
+ * {@code null}:
+ * <pre>{@code
+ *  JsonParser parser = new JsonParser();
+ *  SOData data = parser.parseReader(new StringReader("{...}"));
+ *
+ *  data.getContainer("some_key")
+ *          .ifExists()
+ *          .requireNotNull()
+ *          .process((Object o) -> System.out.println("some_key: " + o));
+ * }</pre>
+ *
+ * @param <K> key type of the {@link #getParentData() parent data}.
+ * @param <V> values type of the {@link #getParentData() parent data}.
+ * @param <O> type of the contained object.
+ */
 @SuppressWarnings("unused")
 public interface Container<K, V, O> extends OptionalValue<O> {
 
+    /**
+     *
+     * @return {@link K key} of the original values {@link #getParentData() parent data}.
+     */
     @NotNull K getKey();
 
+    /**
+     *
+     * @return {@link AbstractData parent data}.
+     */
     @NotNull AbstractData<K, V> getParentData();
 
+    /**
+     * Creates a new {@link Container} with given value.
+     * @param newValue the new value
+     * @return a new {@link  Container}
+     * @param <N> the type of the new value
+     */
     @ApiStatus.Internal
     @NotNull <N> Container<K, V, N> createNewContainer(@Nullable N newValue);
 
+    /**
+     * Creates a new {@link ListContainer} with given value.
+     * @param newValue the list value
+     * @return a new {@link ListContainer}
+     * @param <T> the list-element type
+     */
     @ApiStatus.Internal
     @NotNull <T> ListContainer<T> createNewListContainer(@Nullable List<T> newValue);
 
+    /**
+     *
+     * @param supplier {@link ExceptionSupplier} to supply an exception of your choice.
+     * @return this
+     * @param <E> exception thrown if the value is {@code null}.
+     * @throws E if the value is {@code null} ({@link #get()} returns {@code null}).
+     */
     default <E extends Throwable> @NotNull Container<K, V, O> requireNotNull(ExceptionSupplier<K, AbstractData<K,V>, E> supplier) throws E {
         if(get() == null) throw supplier.supply(getParentData(), getKey());
         return this;
     }
 
+    /**
+     *
+     * @return this
+     * @throws NullPointerException if the value is {@code null} ({@link #get()} returns {@code null}).
+     */
     default @NotNull Container<K, V, O> requireNotNull() throws NullPointerException {
         if(get() == null) throw new NullPointerException(getKey() + " is null.");
         return this;
     }
 
+    /**
+     * Casts the value to {@link List} of {@link Object Objects} and returns a {@link ListContainer} with this list.
+     * @return {@link ListContainer} as specified above.
+     * @throws ClassCastException if the value is not a {@link List} of {@link Object Objects}.
+     */
     @SuppressWarnings("unchecked")
     default @NotNull ListContainer<Object> asList() {
         return createNewListContainer((List<Object>) get());
     }
 
+    /**
+     * Casts the value to {@link C}.
+     * @return a new {@link Container} with the new cast value.
+     * @param <C> type to cast to.
+     * @throws ClassCastException if the value is not of type {@link C}.
+     */
+    @SuppressWarnings("unchecked")
+    default <C> @NotNull Container<K, V, C> cast() {
+        return createNewContainer((C) get());
+    }
+
+    /**
+     * Casts the value to {@link C} and converts it with given {@code converter}.
+     * @param converter the converter to {@link Converter#convert(Object)} from {@link C} to {@link R}.
+     * @return a new {@link Container} with the new cast and converted value.
+     * @param <C> type to cast to.
+     * @param <R> type to {@link Converter#convert(Object)} to.
+     * @throws ClassCastException if the value is not of type {@link C}.
+     */
     @SuppressWarnings("unchecked")
     default <C, R> @NotNull Container<K, V, R> castAndConvert(@NotNull Converter<C, R> converter) {
         return createNewContainer(converter.convert((C) get()));
     }
 
+    /**
+     * Casts the value to {@link C} and converts it with given {@code converter}. The converter can throw an exception.
+     * @param converter the converter to {@link ExceptionConverter#convert(Object)} from {@link C} to {@link R}.
+     * @return a new {@link Container} with the new cast and converted value.
+     * @param <C> type to cast to.
+     * @param <R> type to {@link Converter#convert(Object)} to.
+     * @param <E> exception your converter may throw.
+     * @throws E if your converter throws this exception
+     */
     @SuppressWarnings("unchecked")
     default <C, R, E extends Throwable> @NotNull Container<K, V, R> castAndConvertWithException(@NotNull ExceptionConverter<C, R, E> converter) throws E {
         return createNewContainer(converter.convert((C) get()));
     }
 
+    /**
+     * If the value does <b>not</b> {@link #exists() exist}, a new {@link NonExistentContainer} will be returned.
+     * All Operations on this container will have no effect. This means:
+     * <ul>
+     *     <li>
+     *         {@link #get()} will throw a {@link NonExistentException}.
+     *     </li>
+     *     <li>
+     *         {@link #process(Consumer)} will not execute the given {@link Consumer}.
+     *     </li>
+     *     <li>
+     *         {@link #requireNotNull()} or {@link #requireNotNull(ExceptionSupplier)} will never throw an exception.
+     *     </li>
+     * </ul>
+     * <p>
+     *     If the value does {@link #exists() exist}, the {@link Container} itself (this) is returned.
+     * </p>
+     * <p>
+     *     This method should be mainly used in combination with the {@link #process(Consumer)} method and then functions
+     *     similar to {@link AbstractData#processIfContained(Object, Consumer)}, but can be combined with all other
+     *     container methods.
+     * </p>
+     * @return {@link Container}
+     */
     default @NotNull Container<K, V, O> ifExists() {
         if(exists()) return this;
         return new NonExistentContainer<>(getParentData(), getKey());
     }
 
+    /**
+     * This method is especially useful in combination with {@link #ifExists()} and {@link #requireNotNull()}.
+     * @param consumer {@link Consumer} to process the value.
+     * @return this
+     */
     @SuppressWarnings("UnusedReturnValue")
     default @NotNull Container<K, V, O> process(@NotNull Consumer<O> consumer) {
         consumer.accept(get());
         return this;
     }
-
 }
--- src/main/java/me/linusdev/data/container/ListContainer.java
+++ src/main/java/me/linusdev/data/container/ListContainer.java
@@ -16,6 +16,7 @@
 
 package me.linusdev.data.container;
 
+import me.linusdev.data.AbstractData;
 import me.linusdev.data.OptionalValue;
 import me.linusdev.data.functions.Converter;
 import me.linusdev.data.functions.ExceptionConverter;
@@ -27,12 +28,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * This class holds a {@link List} value from an {@link AbstractData}. It offers
+ * several methods to process the elements of this list and functions similar to it's
+ * single value counterpart: {@link Container}.
+ * @param <T>
+ */
 @SuppressWarnings("unused")
 public interface ListContainer<T> extends OptionalValue<List<T>> {
 
+    /**
+     * Creates a new {@link ListContainer container} with given value.
+     * @param newValue the value for the new container
+     * @return a new {@link ListContainer}.
+     * @param <N> the new value type.
+     */
     @ApiStatus.Internal
     <N> @NotNull ListContainer<N> createNew(@Nullable List<N> newValue);
 
+    /**
+     * Casts each element of the list to {@link C}.
+     * @return a new {@link ListContainer} containing the {@link List} of {@link C}.
+     * @param <C> the type to cast to.
+     * @throws ClassCastException if any element inside this container's list cannot be cast to {@link C}.
+     */
     @SuppressWarnings("unchecked")
     default <C> @NotNull ListContainer<C> cast() {
         if(isNull()) return createNew(null);
@@ -44,10 +63,28 @@ public interface ListContainer<T> extends OptionalValue<List<T>> {
         return createNew(converted);
     }
 
+    /**
+     * Casts each element of the list to {@link C} and then converts it with given converter.
+     * @param converter {@link Container} to convert from {@link C} to {@link R}.
+     * @return a new {@link ListContainer} containing the {@link List} of {@link R}.
+     * @param <C> type to cast to.
+     * @param <R> type to convert to.
+     * @throws ClassCastException if any element inside this container's list cannot be cast to {@link C},
+     */
     default <C, R> @NotNull ListContainer<R> castAndConvert(@NotNull Converter<C, R> converter) {
         return castAndConvertWithException(converter);
     }
 
+    /**
+     * Casts each element of the list to {@link C} and then converts it with given converter.
+     * @param converter {@link Container} to convert from {@link C} to {@link R}.
+     * @return a new {@link ListContainer} containing the {@link List} of {@link R}.
+     * @param <C> type to cast to.
+     * @param <R> type to convert to.
+     * @param <E> exception type of your {@link ExceptionConverter}.
+     * @throws ClassCastException if any element inside this container's list cannot be cast to {@link C}.
+     * @throws E if your converter throws this exception.
+     */
     @SuppressWarnings("unchecked")
     default <C, R, E extends Throwable> @NotNull ListContainer<R> castAndConvertWithException(@NotNull ExceptionConverter<C, R, E> converter) throws E {
         if(isNull()) return createNew(null);
@@ -59,6 +96,11 @@ public interface ListContainer<T> extends OptionalValue<List<T>> {
         return createNew(converted);
     }
 
+    /**
+     * Process this container's list with given {@link Consumer}.
+     * @param consumer to consume this list.
+     * @return this
+     */
     default @NotNull ListContainer<T> process(@NotNull Consumer<List<T>> consumer) {
         consumer.accept(get());
         return this;
